9c732db8a8d2a82f4f7ba6c4a755adfa4531e5aa
JillCarty <jillrcarty@gmail.com>
2015-08-28 08:54:02 -0600

    Update CONFIG.js

--- public/CONFIG.js
+++ public/CONFIG.js
@@ -35,21 +35,21 @@ window.ACTIVITY_IMAGES = {
 
 // Images for event creator
 window.CREATOR_IMAGES = {
-	'Jill Carty': '<img src="/img/jill-image.jpg" class="creator-image">'
-/*	'Roots Elementary':
-	'Anna Stringfield':
-	'Leksy Wolk':
-	'Mackenzie Wagner':
-	'Julia Quintanilla':
-	'Eve Bunevich': 
-	'Jonathan Hanover':
-	'Martin Cech':
-	'Megan Miles':
-	'Samantha Gambino':
-	'Mahdyeh Nowkhandan':
-	'Debbie Van Roy':
-	'Dominic Hernandez':
-	'Idali Franco':*/
+	'Jill Carty': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/jill.jpg" class="creator-image">'
+//	'Roots Elementary':
+	'Anna Stringfield': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/anna.jpg" class="creator-image">'
+	'Leksy Wolk': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/leksy.jpg" class="creator-image">'
+	'Mackenzie Wagner': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/mackenzie.jpg" class="creator-image">'
+	'Julia Quintanilla': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/julia.jpg" class="creator-image">'
+	'Eve Bunevich': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/eve.jpg" class="creator-image">'
+	'Jonathan Hanover': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/jon.jpg" class="creator-image">'
+	'Martin Cech': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/marty.jpg" class="creator-image">'
+	'Megan Miles': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/megan.jpg" class="creator-image">'
+//	'Samantha Gambino': 
+	'Mahdyeh Nowkhandan': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/mahdyeh.jpg" class="creator-image">'
+	'Debbie Van Roy': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/debbie.jpg" class="creator-image">'
+	'Dominic Hernandez': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/dominic.jpg" class="creator-image">'
+	'Idali Franco': '<img src="http://rootselementary.org/wp-content/uploads/2015/08/idali.jpg" class="creator-image">'
 }
 
 // The list of all possible grove calendar activities
