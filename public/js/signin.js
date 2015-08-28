64823c1cffc2c4a7ad27bdfdafe0b19601bd0b3f
JillCarty <jillrcarty@gmail.com>
2015-08-28 13:39:33 -0600

    Update signin.js

--- public/js/signin.js
+++ public/js/signin.js
@@ -8,7 +8,7 @@ renderProgressBar = function(eventStart){
     start_time: currentTime, //Time when the progress bar is at 0%
     end_time: eventStart || currentTime.add(1, ms), //Time Progress bar is at 100% and timer runs out, when no eventStart is passed for end_time, use current time with 1 added ms to trigger onComplete and update_progress
     progress: $('.progress-bar'), //There dom element which should display the progressbar.
-    onComplete: function() {lo
+    onComplete: function() {
       $('.timer').show();
       $('.timer').replaceWith("<div class=\"timer ended\">Time's Up!</div>");
     },
