2072fc55e63d3033ef2c8fdcb928f5557ffdd2d8
JillCarty <jillrcarty@gmail.com>
2015-08-29 10:44:06 -0600

    Update CONFIG.js

--- public/CONFIG.js
+++ public/CONFIG.js
@@ -1,5 +1,5 @@
 // Set the event length. 10 minutes * 60 seconds * 1000 ms. 
-window.EVENT_LENGTH = 10 * 60 * 1000;
+window.EVENT_LENGTH = 5 * 60 * 1000;
 
 // Transition time between events. 2 minutes * 60s * 1000 ms.
 window.TRANSITION_LENGTH = 2 * 60 * 1000;
