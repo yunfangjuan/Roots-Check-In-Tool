b7fe31f51edf7e17106e0c92528f5ef2d4d8ebe1
JillCarty <jillrcarty@gmail.com>
2015-09-08 23:33:33 -0600

    Update lost-kids.js

--- public/js/lost-kids.js
+++ public/js/lost-kids.js
@@ -261,7 +261,7 @@ $(function(){
 	$.get('api/user', function(students) {
 		studentsArray = _.map(students, function(student) {
 			return new StudentLocationDisplay(student);
-			this = sort('data.name',studentsArray);
+			sort('data.name',studentsArray);
 		});
 
 		// Put in a slight delay for student panels to display, then set them all to same height
